635ad6f88c3ebd1c59f34b0895711d19da6f938f
Frank.Q.Shan <frankqs0304@gmail.com>
2016-11-03 21:51:15 +0800

    intial version

---
+++ src/main.cpp
@@ -0,0 +1,50 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Oct 26, 2016
+ *      Author: fshan
+ */
+
+#include <stdio.h>
+#include <iostream>
+
+
+int main()
+{
+	int inputdata;
+#if 1
+//exmaple the standard cout/cin/endl
+	std::cout << "Hello cpp!" << std::endl;
+	std::cout << "Please input your data: " << std::endl;
+	std::cin >> inputdata;
+	std::cout << "your inputdata is: " <<inputdata << std::endl;
+#endif
+
+#if 1
+//example of specifying the cout/cin/endl
+	using std::cout;
+	using std::endl;
+	using std::cin;
+	cout << "Hello cpp!" << std::endl;
+	cout << "Please input your data: " << std::endl;
+	cin >> inputdata;
+	cout << "your inputdata is: " <<inputdata << std::endl;
+#endif
+
+
+
+#if 1
+//example of specifying the namespace
+	using namespace std;
+	cout << "Hello cpp!" << std::endl;
+	cout << "Please input your data: " << std::endl;
+	cin >> inputdata;
+	cout << "your inputdata is: " <<inputdata << std::endl;
+#endif
+
+
+
+
+
+	return 0;
+}
