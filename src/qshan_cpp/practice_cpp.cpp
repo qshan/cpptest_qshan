ecef0f9a7269eca1ae70641fe27ceec8355495c6
Frank,shan <frankqs0304@hotmail.com>
2017-09-04 12:29:08 +0800

    add the inherit the function of parent class example, worked

--- src/qshan_cpp/practice_cpp.cpp
+++ src/qshan_cpp/practice_cpp.cpp
@@ -55,9 +55,15 @@ public:
   void set_name(string u_name);
   void display_name(void);
   string get_name(void);
+  person_test_qshan(string u_name, int u_old);
 };
 
-
+person_test_qshan::person_test_qshan(string u_name, int u_old)
+{
+  name = u_name;
+  old = u_old;
+  cout << "##this the construct function of class person_test_qshan##" << std::endl;
+}
 void person_test_qshan::set_name(string u_name)
 {
   name = u_name;
@@ -84,10 +90,11 @@ public:
   son_test_qshan (string u_name, int u_old, string u_job);
 };
 
-son_test_qshan::son_test_qshan (string u_name, int u_old, string u_job)
+//##### inherit the function from the construct function of parent class !!!
+son_test_qshan::son_test_qshan (string u_name, int u_old, string u_job):person_test_qshan(u_name, u_old)
 {
-  name = u_name;
-  old = u_old;
+  //name = u_name;
+  //old = u_old;
   job = u_job;
 }
 
@@ -111,7 +118,7 @@ void class_test_qshan(void)
 
 
   //person_test_qshan test_person_test_qshan("input", 0);
-  person_test_qshan test_person_test_qshan;
+  person_test_qshan test_person_test_qshan("Xiaoxiao", 10);
 
   cout << std::endl  << std::endl;
   test_person_test_qshan.set_name("Xiao-xiao");
@@ -121,7 +128,7 @@ void class_test_qshan(void)
   son_test_qshan test_son_test_qshan("input", 10, "input");
 
   cout << std::endl  << std::endl;
-  test_son_test_qshan.set_name("Xiaoxiao");
+  test_son_test_qshan.set_name("XiaoXiao");
   test_son_test_qshan.display_name();
   cout << std::endl;
   test_son_test_qshan.set_job("Student");
